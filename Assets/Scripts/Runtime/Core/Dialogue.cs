842e722d30b8b5e2c2d04bd20008fe2e440ab884
Frank <tim4224171@gmail.com>
2024-01-27 00:25:18 +0800

    add dialogue script

---
+++ Assets/Scripts/Runtime/Core/Dialogue.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+namespace Runtime.Core
+{
+    public class Dialogue : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI showText;
+        [SerializeField] private string[]        sentences;
+
+        private int _sentencesLength;
+        private int _loadingTextNum = -1;
+
+        private void Start()
+        {
+            _sentencesLength = sentences.Length;
+
+            InvokeRepeating(nameof(PlayingSentence), 0, 10);
+        }
+
+        private void PlayingSentence()
+        {
+            if (_loadingTextNum + 1 < _sentencesLength)
+            {
+                _loadingTextNum++;
+                StartCoroutine(LoadingText(sentences[_loadingTextNum]));
+            }
+            else
+            {
+                // Debug.Log("超出選擇");
+            }
+        }
+
+        private IEnumerator LoadingText(string text)
+        {
+            string swapStr   = "";
+            int    loopTimes = text.Length;
+            for (int i = 0; i < loopTimes; i++)
+            {
+                swapStr       += text[i];
+                showText.text =  swapStr;
+                yield return new WaitForSeconds(.1f);
+                // Debug.Log(swapStr);
+            }
+
+            // Debug.Log("顯示結束");
+        }
+    }
+}
